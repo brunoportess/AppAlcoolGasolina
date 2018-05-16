2ebdafebf52370dac1ca65ff3400347a8c36302e
Bruno Portes <bruno.portess@hotmail.com>
2018-05-15 22:57:20 -0300

    Ajuste de behavior

---
+++ AlcoolGasolina/AlcoolGasolina/Behaviors/MoneyBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AlcoolGasolina.Behaviors
+{
+    public class MoneyBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == null || Convert.ToDouble(e.NewTextValue) == Convert.ToDouble(e.OldTextValue) / 100) return;
+            var valor = Convert.ToDouble(e.NewTextValue.Replace(".", ""));
+            valor = valor / 100;
+            ((Entry)sender).Text = valor.ToString();
+        }
+    }
+}
